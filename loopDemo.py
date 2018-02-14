0ebeb921fcfa396e30bdf82340f7763cd89fbe61
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-02-14 08:31:53 -0500

    Create loopDemo.py

---
+++ loopDemo.py
@@ -0,0 +1,59 @@
+#Matt Westelman
+#loopDemo.py - how to use for and while loops
+#2/14/18
+
+"""
+#I love comp sci 5x
+for i in range(1,6):
+    print("I <3 Computer Science")
+"""
+
+"""
+#using a while loop
+i=0
+while i<5:
+    print("I <3 Computer Science")
+"""
+
+"""
+i = 1
+while i<=10:
+    print(i)
+    i+=1 #adding 1 to i
+"""
+
+"""
+#Counts  from 1 to 10
+for i in range(1,11):
+    print(i)
+"""
+
+"""
+#Counting 27 to 43 by 2s
+for i in range(27,44,2):
+    print(i)
+"""
+
+"""
+i = 27
+while i <= 43:
+    print(i)
+    i+=2
+"""
+
+"""
+#add numbers 1 to 5
+total = 0
+for i in range(1,6):
+    total = total + i
+print(total)
+"""
+
+"""
+total = 0
+i = 1
+while i <=5:
+    total = total + i
+    i+=1
+print(total)
+"""
