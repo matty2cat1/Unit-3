63d1d8f08e7eb8e773dbe6e33c88f5eec6394a4a
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-02-14 08:39:29 -0500

    Create gauss.py

---
+++ gauss.py
@@ -0,0 +1,10 @@
+#Matt Westelman
+#gauss.py - adding lots of numbers
+#2/14/18
+
+num = int(input("Please enter a number"))
+num = int(input("Enter another!")
+
+
+for i in range(num,num2+1,diff):
+    print("*"*i)
