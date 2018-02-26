46ffcbe058529da2fc6637d1cbea420cf916887a
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-02-26 08:27:36 -0500

    Create favWord.py

---
+++ favWord.py
@@ -0,0 +1,8 @@
+#Matt Westelman
+#2/26/18
+#favWord.py - what is your favorite word?
+
+word = input("please enter your favorite word ")
+num = int(input("enter your favorite number"))
+for i in range (1,num+1):
+    print(word)
