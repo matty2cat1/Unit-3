6d67169c897e94f4e6134c3bd980872f2d90ad0d
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-02-28 08:22:29 -0500

    Update betterAdditonGameDemo.py

--- betterAdditonGameDemo.py
+++ betterAdditonGameDemo.py
@@ -1,1 +1,18 @@
+#Matt Westelman
+#2/28/18
+#betterAdditonGameDemo.py  - more loops
 
+from random import randint
+
+numCorrect = 0
+while numCorrect < 5:
+    num1 = randint(-10,10)
+    num2 = randint(-10,10)
+    answer= int(input("What is " + str(num1) + " + " + str(num2) + "? "))
+    if answer == num1 + num2:
+        numCorrect += 1
+    else:
+        while true:
+        print("wrong")
+
+print("you win!")
