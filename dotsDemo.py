47172102194dce9dc65b5b1b85bbff6118a38238
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-03-01 08:18:28 -0500

    Update dotsDemo.py

--- dotsDemo.py
+++ dotsDemo.py
@@ -1,1 +1,50 @@
+#Matt Westelman
+#3/1/2018
+#dotsDemo.py - loops n' graphics
+
+from ggame import *
+
+RADIUS = 10
+
+blackOutline = LineStyle(1,black)
+red = Color(0xFF0000,1) #this is the color red
+green = Color(0x00FF00,1)#green
+
+
+dot = CircleAsset(RADIUS,blackOutline,green) #Radius, outline, fill
+for i in range(12): #a row of dots!
+    for j in range(12):
+        Sprite(dot,(10+(2*RADIUS+10)*i,10+(2*RADIUS+10)*j))
+        
+App().run()
+
+
+""" Archive of graphics
+red = Color(0xFF0000,1) #this is the color red
+green = Color(0x00FF00,1)#green
+blue = Color(0x0000FF,1) #blue
+black = Color(0x000000,1) #Black
+maple = Color(0xD2691E,1) #maplewood
+aqua = Color(0x00FFFF,1) #Aqua
+yellow = Color(0xFFFF00,1) #yellow
+color = Color(colorCod,1)
+
+blackOutline = LineStyle(1,black)
+yellowOutline= LineStyle(1,yellow)
+redOutline= LineStyle(1,red)
+
+redRectangle = RectangleAsset(450,100,redOutline,red) #Width, height, outline and fill
+orangeRectangle = RectangleAsset(10,20,blackOutline, maple) #DOOR
+blueCircle = CircleAsset(50,blackOutline,blue) #Radius, outline, fill
+greenEllipse = EllipseAsset(100, 50, blackOutline, green) #Width, height, outline, fill
+blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, lineStyle
+blueTriangle = PolygonAsset([(30,30), (60,52), (0,52)], blackOutline,blue) #endpoints, outline, fill
+aquaRectangle = RectangleAsset(10,10,blackOutline,aqua)
+blackRectangle = RectangleAsset(450,100,blackOutline,black)
+yellowRectangle = RectangleAsset(450,100,yellowOutline,yellow)
+stupidThiccRectangle = RectangleAsset(800,600,blackOutline,color)
+App().run()
+"""
+
+
 
