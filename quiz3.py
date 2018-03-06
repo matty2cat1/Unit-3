4474da074def7425d07a18d4d1bb16526db36fd7
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-03-06 08:31:07 -0500

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -2,6 +2,7 @@
 #3/7/2018
 #quiz3.py - a quiz on unit 3
 
+total=0
 i=-15
 while i<-8:
     print(i)
@@ -10,3 +11,13 @@ while i<-8:
 n=50
 for n in range(50,17,-4):
     print(n)
+    
+b=-100
+for b in range(-100,1001,2):
+    total=total+b
+print(total)
+
+while 1>0:
+    text = input("Enter text ")
+    if "alpaca" in text:
+        break
